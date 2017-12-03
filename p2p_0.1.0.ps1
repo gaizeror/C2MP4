893e60e5b1ad9c4d639499ffa7cf5bdffe02695f
gaizeror <gaizeror@gmail.com>
2017-12-04 00:36:50 +0200

    Created generic script that supports multiple sites
    Tested on Pluralsight and udemy.

--- p2p_0.1.0.ps1
+++ p2p_0.1.0.ps1
@@ -1,218 +1,98 @@
-﻿$title = "PS offline To Arranged Directory"
-$message = "Do you want to create a copy of the videos or move the videos to a new location?"
+function textbox ($purpose){
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
 
-$Move = New-Object System.Management.Automation.Host.ChoiceDescription "&Move", `
-    "Deletes all the files in the folder."
+    $form = New-Object System.Windows.Forms.Form 
+    $form.Text = "Data Entry Form"
+    $form.Size = New-Object System.Drawing.Size(300,200) 
+    $form.StartPosition = "CenterScreen"
 
-$Copy = New-Object System.Management.Automation.Host.ChoiceDescription "&Copy", `
-    "Retains all the files in the folder."
+    $OKButton = New-Object System.Windows.Forms.Button
+    $OKButton.Location = New-Object System.Drawing.Point(75,120)
+    $OKButton.Size = New-Object System.Drawing.Size(75,23)
+    $OKButton.Text = "OK"
+    $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $OKButton
+    $form.Controls.Add($OKButton)
 
+    $CancelButton = New-Object System.Windows.Forms.Button
+    $CancelButton.Location = New-Object System.Drawing.Point(150,120)
+    $CancelButton.Size = New-Object System.Drawing.Size(75,23)
+    $CancelButton.Text = "Cancel"
+    $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.CancelButton = $CancelButton
+    $form.Controls.Add($CancelButton)
 
-$options = [System.Management.Automation.Host.ChoiceDescription[]]($Move, $Copy)
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point(10,20) 
+    $label.Size = New-Object System.Drawing.Size(280,20) 
+    $label.Text = "Please enter the " + $purpose + " here:"
+    $form.Controls.Add($label) 
 
-$result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+    $textBox = New-Object System.Windows.Forms.TextBox 
+    $textBox.Location = New-Object System.Drawing.Point(10,40) 
+    $textBox.Size = New-Object System.Drawing.Size(260,20) 
+    $form.Controls.Add($textBox) 
 
-$sourcePath = "$env:USERPROFILE\AppData\Roaming\Pluralsight\videos\"
-do
-{    
-$items = Get-ChildItem -Path $sourcePath -Recurse -Filter "*.mp4" | sort LastWriteTime | Select LastWriteTime
- 
-# enumerate the items array
-foreach ($item in $items)
-{
-            Write-Host $item.LastWriteTime
-}
+    $form.Topmost = $True
 
-Write-Host "Look at the time up in the table and select the first and last files"
-Write-Host "If you will write the date only, the time will be 00:00:00"
+    $form.Add_Shown({$textBox.Select()})
+    $result = $form.ShowDialog()
 
-$date= read-host "Please enter date & time of the fisrt file downloaded on this course (ie: 'MM/DD/YY 09:00:00'):"
-
-$date = $date -as [datetime]
-
-if (!$date) { "Not A valid date and time"}
-
-} while ($date -isnot [datetime])
-
-$date
-
-$begin = $date
-
-do
-{    
-$date= read-host "Please enter date & time of the last file downloaded on this course (ie: 'MM/DD/YY 09:00:59'):"
-
-$date = $date -as [datetime]
-
-if (!$date) { "Not A valid date and time"}
-
-} while ($date -isnot [datetime])
-
-$date
-
-$end = $date
-
-#End of part 1
-
-$pathsList = @()
-
-$files = Get-ChildItem $sourcePath -Recurse -Filter "*.mp4" |Where-Object { $_.LastWriteTime -gt $begin -and $_.LastWriteTime -lt $end}| sort LastWriteTime |select FullName   
-foreach ($item in $files)
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
     {
-     $pathsList += $item.FullName
-    }
-
-#End of part 2
-Write-Host "Please select the csv file which maps the videos and directories"
-
-Function Get-FileName($initialDirectory)
-{
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-    
-    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-    $OpenFileDialog.initialDirectory = $initialDirectory
-    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
-    $OpenFileDialog.ShowDialog() | Out-Null
-    $OpenFileDialog.filename
-}
-
-$selection = Get-FileName
-$csvFile = Import-Csv $selection | where {$_.directory -match "[a-zA-Z]" -and $_.videos -match "[a-zA-Z]"}
-
-
-# End of part 3
-
-$regex = '(\d+m\s\d+s)'
-$videosCount = $csvFile.videos.Length
-$directoryCount = $csvFile.directory.Length
-if (($videosCount -ne $pathsList.Length) -or ($videosCount -ne $directoryCount))
-{
-    [System.Windows.Forms.MessageBox]::Show("there are " + $pathsList.Length + " videos and " + $videosCount + " video names in the csv
-    and " + $directoryCount + " directories(same or different) int the csv file. Please fix it and try again later")
-    exit
-}
-
-Write-Host "Please Select The Directory For The New Files"
-Function Select-FolderDialog
-{
-    param([string]$Description="Select Folder",[string]$RootFolder="Desktop")
-
- [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") |
-     Out-Null     
-
-   $objForm = New-Object System.Windows.Forms.FolderBrowserDialog
-        $objForm.Rootfolder = $RootFolder
-        $objForm.Description = $Description
-        $Show = $objForm.ShowDialog()
-        If ($Show -eq "OK")
-        {
-            Return $objForm.SelectedPath
-        }
-        Else
-        {
-            Write-Error "Operation cancelled by user."
-        }
-}
-
-$destDir = Select-FolderDialog
-
-$directories = @($csvFile.directory[0])
-
-for ($i = 1; $i -le $csvFile.directory.length - 1; $i++)
-{
-    if ($csvFile.directory[$i] -ne $directories[-1])
-    {
-        $directories += $csvFile.directory[$i]
+        $x = $textBox.Text
+        $x
     }
 }
 
-foreach ($newFolder in $directories)
-{
-   
-   New-Item "$destDir\$newFolder" -ItemType directory
-}
-Function Remove-InvalidFileNameChars {
-  param(
-    [Parameter(Mandatory=$true,
-      Position=0,
-      ValueFromPipeline=$true,
-      ValueFromPipelineByPropertyName=$true)]
-    [String]$Name
-  )
-
-  $invalidChars = [IO.Path]::GetInvalidFileNameChars() -join ''
-  $re = "[{0}]" -f [RegEx]::Escape($invalidChars)
-  return ($Name -replace $re)
-}
+$url = textbox ("course's URL")
+$user = textbox ("site's username")
+$password = textbox ("site's password")
 
 
-switch ($result)
+function Find-Folders {
+    [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+    [System.Windows.Forms.Application]::EnableVisualStyles()
+    $browse = New-Object System.Windows.Forms.FolderBrowserDialog
+    $browse.SelectedPath = "C:\"
+    $browse.ShowNewFolderButton = $false
+    $browse.Description = "Select the target directory"
+
+    $loop = $true
+    while($loop)
     {
-        0 
+        if ($browse.ShowDialog() -eq "OK")
         {
-            $serialNum = 0
-            $lastDir = $csvFile.directory[0]
-            for ($i=0; $i -le $csvFile.videos.Length - 1; $i++)
+        $loop = $false
+		
+		#Insert your script here
+		
+        } else
+        {
+            $res = [System.Windows.Forms.MessageBox]::Show("You clicked Cancel. Would you like to try again or exit?", "Select a location", [System.Windows.Forms.MessageBoxButtons]::RetryCancel)
+            if($res -eq "Cancel")
             {
-                if ($csvFile.videos[$i] -match $regex)
-                {
-                    $video = $csvFile.videos[$i] -replace $regex,""
-                }
-                if ($csvFile.directory[$i] -eq $lastDir)
-                {
-                    $serialNum++
-                }
-                    else
-                    {
-                        $serialNum=1
-                        $lastDir = $csvFile.directory[$i]
-                    }
-                    $newdir = $csvFile.directory[$i]
-                $video = Remove-InvalidFileNameChars $video
-                Move-Item $pathsList[$i] "$destDir\$newdir\$serialNum. $video.mp4"
-                     
+                #Ends script
+                return
             }
-          }
-    
-    
-        1 
-        {
-            "You selected Copy."
-            $serialNum = 0
-            $lastDir = $csvFile.directory[0]
-            for ($i=0; $i -le $csvFile.videos.Length - 1; $i++)
-            {
-                if ($csvFile.videos[$i] -match $regex)
-                {
-                    $video = $csvFile.videos[$i] -replace $regex,""
-                }
-                if ($csvFile.directory[$i] -eq $lastDir)
-                {
-                    $serialNum++
-                }
-                    else
-                    {
-                        $serialNum=1
-                        $lastDir = $csvFile.directory[$i]
-                    }
-                    $newdir = $csvFile.directory[$i]
-                $video = Remove-InvalidFileNameChars $video
-                Copy-Item $pathsList[$i] "$destDir\$newdir\$serialNum. $video.mp4"
-            }
-           
         }
-    }
+    } 
+    $browse.SelectedPath
+    $browse.Dispose()
+} 
 
-    $dirs = Get-ChildItem -Path $destDir | sort CreationTime |select Name 
-    $i = 0
-    foreach ($dir in $dirs)
-    {
-        $i++
-        $newname = $dir.Name
-        Rename-Item "$destDir\$newname" -NewName "$destDir\$i. $newname"
-        # Write-Host "$destDir\$newname" -NewName "$destDir\$i. $newname"
-    }
-    
-    Write-Host "Done. Press any key to continue"
+$directory= Find-Folders
 
-    $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+cd $directory
+youtube-dl -u $user -p $password $url
+
+
+$list=get-childitem -Path $directory -filter "*.mp4"| sort CreationTime |Select-Object -ExpandProperty name
+$i=1;
+foreach ($file in $list)
+{
+    Rename-Item -Path $directory\$file -NewName $i'.'$file
+    $i++;
+}
+get-childitem -Path $directory -filter "*"| sort CreationTime |Select-Object -ExpandProperty name
